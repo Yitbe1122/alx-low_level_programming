39044f06efa60aab90f4b9ca1da0b4211b7eaf77
yitbe1122 <yitbehiwot87@gmail.com>
2022-03-15 14:36:12 +0300

    understand the commandline

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,50 @@
-#include "main.h"
+/*
+ * File: 100-times_table.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
 
 /**
- * print_times_table - prints nx times table
- * @n: value of how large the times table should be
- *
- * Return: void
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-	if (n < 16 && n > -1)
-	{
-		int column, row, sum, h, t, o;
+	int num, mult, prod;
 
-		column = 0;
-		while (column <= n)
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
-			row = 1;
-			while (row <= n)
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				sum = column * row;
-				h = sum / 100, t = sum / 10 % 10, o = sum % 10;
 				_putchar(',');
 				_putchar(' ');
-				if (sum > 99)
-				{
-					_putchar(h + '0');
-					_putchar(t + '0');
-					_putchar(o + '0');
-				}
-				else if (sum < 10)
-				{
+
+				prod = num * mult;
+
+				if (prod <= 99)
 					_putchar(' ');
+				if (prod <= 9)
 					_putchar(' ');
-					_putchar(o + '0');
-				}
-				else
+
+				if (prod >= 100)
 				{
-					_putchar(' ');
-					_putchar(t + '0');
-					_putchar(o + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				row++;
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
+		}
+	}
+}
 
