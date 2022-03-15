0dc9fdeff6f863f14d4fa008353cfd55fc7efa39
yitbe1122 <yitbehiwot87@gmail.com>
2022-03-15 13:35:39 +0300

    aad.c commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add-finds the sum of two numbers.
+ *@a:first value to add to a second value.
+ *@b:second value added to first.
+ *
+ *Return:returns the sum.
+ */
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
