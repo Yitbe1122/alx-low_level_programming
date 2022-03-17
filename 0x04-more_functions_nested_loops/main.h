49cc6067ef27284e0556ab92ae0817b055db03b1
Yitbe1122 <99283305+Yitbe1122@users.noreply.github.com>
2022-03-17 20:14:30 +0300

    new

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-© 2022 GitHub, Inc.
-Terms
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(void);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+#endif /* HOLBERTON_H */
