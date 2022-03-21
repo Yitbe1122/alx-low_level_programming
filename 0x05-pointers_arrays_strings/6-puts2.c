8bbfd484132c5943b669b8d72e36bef7e4e80cd8
yitbe1122 <yitbehiwot87@gmail.com>
2022-03-21 11:58:33 +0300

    new updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,16 @@
 #include "main.h"
 
-/**
- * puts2 - prints multiples of two.
- * @str: first parameter
- *
- * Return: void
- */
 void puts2(char *str)
 {
-	int i, len, start;
+	int i = 0;
 
-	len = 0;
-
-	for (i = 0; str[i] != '\0'; ++i)
+	while (*(str + i) != '\0')
 	{
-		len = i;
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		i++;
 	}
-
-	for (start = 0; start <= len; start++)
-	{
-		if (start % 2 == 0)
-			_putchar(str[start]);
-		else
-			continue;
-	}
-
 	_putchar('\n');
 }
