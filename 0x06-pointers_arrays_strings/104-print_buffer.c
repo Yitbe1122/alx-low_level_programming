be9800851d0e974fd71b56d5748e0f7be1c54e85
yitbe1122 <yitbehiwot87@gmail.com>
2022-03-23 21:47:24 +0300

    new updated

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,12 +1,18 @@
-void print_buffer(CSV_BUFFER *buffer)
+#include <nonstdio.h>
+
+void
+print_buffer(uint32_t *buf, size_t n)
 {
-        int i, j;
-        printf("\n");
-        for (i = 0; i < buffer->rows; i++) {
-        for (j = 0; j < buffer->width[i]; j++) {
-                printf("%c%s%c%c", buffer->text_delim, buffer->field[i][j]->text, buffer->text_delim, buffer->field_delim);
-        }
-        printf("\n");
-        }
-        printf("\n\n");
+  size_t i;
+  for (i = 0; i < n; i++){
+    if (i % 4 == 0)
+      puthex16(i * 4);
+
+    putchar(' ');
+    puthex32(buf[i]);
+    if (i % 4 == 3)
+      newline();
+  }
+
+  newline();
 } 
