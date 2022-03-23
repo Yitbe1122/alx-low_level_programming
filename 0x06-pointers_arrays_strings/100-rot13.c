68f519dc776224fd0078e0a9e0c2ea713d8afaff
yitbe1122 <yitbehiwot87@gmail.com>
2022-03-23 19:32:35 +0300

    new updated

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "rot13.h"
+
+/*
+ * Magic number.
+ */
+
+#define ROT13_MODIFIER 13
+
+/*
+ * Rotate the given `str` inline.
+ */
+
+char *rot13(char *str) {
+  for (int i = 0; '\0' != str[i]; i++) {
+    char c = *(str + i);
+    if (('a' <= c && 'n' > c) || ('A' <= c && 'N' > c)) {
+      // a-m
+      *(str + i) += ROT13_MODIFIER;
+    } else if (('n' <= c && 'z' >= c) || ('N' <= c && 'Z' >= c)) {
+      // n-z
+      *(str + i) -= ROT13_MODIFIER;
+    }
+  }
+  return str;
+}
