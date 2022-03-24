7fd1674337a99117e86e6df4f32bd3b41f4e5722
yitbe1122 <yitbehiwot87@gmail.com>
2022-03-24 11:29:52 +0300

    new updated

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,18 +1,61 @@
-#include <nonstdio.h>
+#include "main.h"
+#include <stdio.h>
 
-void
-print_buffer(uint32_t *buf, size_t n)
+/**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
 {
-  size_t i;
-  for (i = 0; i < n; i++){
-    if (i % 4 == 0)
-      puthex16(i * 4);
+	int j, k;
 
-    putchar(' ');
-    puthex32(buf[i]);
-    if (i % 4 == 3)
-      newline();
-  }
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
 
-  newline();
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 } 
